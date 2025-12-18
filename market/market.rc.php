c525df0b880d21cc3a83e48c617edf2e996ab739
sodium carbonate <webitproff@gmail.com>
2025-12-18 21:06:03 +0200

    Update market.rc.php

--- market/market.rc.php
+++ market/market.rc.php
@@ -5,13 +5,25 @@ Hooks=rc
 [END_COT_EXT]
 ==================== */
 
-
-
 defined('COT_CODE') or die('Wrong URL');
 
+$useUrlEditorTree = false;
 
-// marketTreeScript
-$marketTreeScriptFile = Cot::$cfg['modules_dir'] . '/market/js/marketTreeScript.js';
-if (file_exists($marketTreeScriptFile)) {
-    Resources::linkFileFooter($marketTreeScriptFile, 'js');
+if (cot_plugin_active('urleditor')) {
+    $preset = $cfg['plugin']['urleditor']['preset'] ?? 'none';
+    if ($preset === 'handy' || $preset === 'myconfig') {
+        $useUrlEditorTree = true;
+    }
+}
+
+if ($useUrlEditorTree) {
+    // URL Editor ON
+    $file = Cot::$cfg['modules_dir'] . '/market/js/marketTreeScriptURLEditor.js';
+} else {
+    // URL Editor OFF (default)
+    $file = Cot::$cfg['modules_dir'] . '/market/js/marketTreeScript.js';
+}
+
+if (file_exists($file)) {
+    Resources::linkFileFooter($file, 'js');
 }
